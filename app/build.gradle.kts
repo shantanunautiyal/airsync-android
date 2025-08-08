1243910658fb4b2535d2bc722ecf8b89fd4c24d5
sameerasw <sameerasw.com@gmail.com>
2025-08-09 03:41:50 +0530

    version upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "2.0.10-BETA"
+        versionName = "2.0.11-BETA"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
