75f47b4de507e7d245f7327b173a0c1a8759aff8
sameerasw <sameerasw.com@gmail.com>
2025-09-02 23:56:07 +0530

    version upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sameerasw.airsync"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0.25-BETA"
+        versionCode = 3
+        versionName = "2.0.28-BETA"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
